0b51ce684686bed36ac29124a72e6041144fd5d1
Iqbal Maulana <iqbal19847@gmail.com>
2025-03-19 05:40:54 +0700

    optimizing

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -45,7 +45,7 @@ export default function Hero() {
                         <a href="#contact" className="text-base font-semibold text-white bg-gray-800 py-3 px-8 rounded-full
                         hover:bg-gray-500 transition duration-300 ease-in-out md:text-xl lg:text-2xl">Contact Me</a>
                     </motion.div>
-                    <motion.div
+                    {/* <motion.div
                         className="w-full self-end px-4 lg:w-1/2"
                         initial={{ x: '100%', opacity: 0 }}
                         animate={isSectionVisible ? { x: 0, opacity: 1 } : { x: '100%', opacity: 0 }}
@@ -53,7 +53,7 @@ export default function Hero() {
                         whileTap={{ scale: 0.95 }}
                         drag
                         dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                        transition={{ duration: 1.5, ease: "easeInOut", type:"spring" }}>
+                        transition={{ duration: 1.5, ease: "easeInOut", type:"spring" }}> */}
                         <div className="mt-10 lg:mt-0 lg:right-0">
                             <Image
                                 src="/iqbal-maulana-pp.png"
@@ -69,7 +69,7 @@ export default function Hero() {
                                 blurDataURL="/path/to/low-quality-image.jpg"
                             />
                         </div>
-                    </motion.div>
+                    {/* </motion.div> */}
                 </div>
             </div>
         </div>
