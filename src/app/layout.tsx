72af562a12501a3c9d085d8a6fc27c084e32a850
Iqbal Maulana <iqbal19847@gmail.com>
2025-03-19 11:27:15 +0700

    optimizing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +37,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-tr from-gray-100  to-gray-500`}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
